0e8812343b0301bada804d3c2a1895ff50147812
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-11-25 23:51:45 +0800

    update

--- devices/bcm27xx_bcm2710/diy.sh
+++ devices/bcm27xx_bcm2710/diy.sh
@@ -5,7 +5,7 @@ bash $SHELL_FOLDER/../common/kernel_5.15.sh
 
 sed -i 's/ factory.img.gz / /' target/linux/bcm27xx/image/Makefile
 
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += fdisk lsblk kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152-vendor/' target/linux/bcm27xx/Makefile
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += fdisk lsblk kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152/' target/linux/bcm27xx/Makefile
 
 mkdir -p files/lib/firmware/brcm
 wget -P files/lib/firmware/brcm/ https://github.com/RPi-Distro/firmware-nonfree/raw/bullseye/debian/config/brcm80211/brcm/brcmfmac43436-sdio.bin
